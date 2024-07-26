21c3af1b2d9416b85f88a5396b5797037d8fb2d7
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-07-26 09:11:34 +0300

    saga orche shared added

---
+++ src/Shared/SagaOrche.Shared/OrderEvents/OrderCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using SagaOrche.Shared.Messages;
+
+namespace SagaOrche.Shared.OrderEvents;
+
+public class OrderCreatedEvent : CorrelatedBy<Guid>
+    {
+        public OrderCreatedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+        public Guid CorrelationId { get; }
+        public List<OrderItemMessage> OrderItems { get; set; }
+    }
---
+++ src/Shared/SagaOrche.Shared/PaymentEvents/PaymentFailedEvent.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using SagaOrche.Shared.Messages;
+
+namespace SagaOrche.Shared.PaymentEvents;
+
+public class PaymentFailedEvent : CorrelatedBy<Guid>
+{
+    public Guid CorrelationId { get; }
+
+    public PaymentFailedEvent(Guid correlationId)
+    {
+        CorrelationId = correlationId;
+    }
+    public string Message { get; set; }
+    public List<OrderItemMessage> OrderItems { get; set; } = default!;
+}
