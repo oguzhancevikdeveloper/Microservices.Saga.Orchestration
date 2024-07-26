3de3407905843ead8e6eea1b13f93eed49adfeb7
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-07-26 14:21:49 +0300

    saga orcge state machine added

---
+++ src/Services/SagaStateMachine.Service/StateMaps/OrderStateMap.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateInstances;
+
+namespace SagaStateMachine.Service.StateMaps;
+
+public class OrderStateMap : SagaClassMap<OrderStateInstance>
+{
+    protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+    {
+        entity.Property(x => x.BuyerId)
+            .IsRequired();
+
+        entity.Property(x => x.OrderId)
+            .IsRequired();
+
+        entity.Property(x => x.TotalPrice)
+            .HasDefaultValue(0);
+    }
+}
